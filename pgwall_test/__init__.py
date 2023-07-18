7972e1b0fe052bd8337f91ca8d430cb46f89501f
pgwall <62671208+pgwall@users.noreply.github.com>
2023-07-18 16:21:36 -0700

    Update __init__.py

--- pgwall_test/__init__.py
+++ pgwall_test/__init__.py
@@ -7,3 +7,5 @@ __email__ = 'pgwall@eng.ucsd.edu'
 __version__ = '0.1.0'
 __repo_url__ = 'https://github.com/pgwall/pgwall_test'
 __description__ = 'Python Boilerplate contains all the boilerplate you need to create a Python package with command line'
+
+from pgwall_test.runner import *
